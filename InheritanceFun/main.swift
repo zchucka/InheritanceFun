74abe03642809890ed2f7a277a9dec328422176e
Chucka, Zachary Tyler <zchucka@hk223-27724.local>
2018-09-13 10:12:02 -0700

    Initial Commit

---
+++ InheritanceFun/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  InheritanceFun
+//
+//  Created by Chucka, Zachary Tyler on 9/13/18.
+//  Copyright © 2018 Chucka, Zachary Tyler. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
